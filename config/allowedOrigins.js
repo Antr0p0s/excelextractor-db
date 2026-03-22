6a935892b494cd9fee42b749360a646b60b943c3
Antr0p0s <jelmersijbers@gmail.com>
2026-03-22 14:54:58 +0100

    whoops

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://excelextractor.randomwebserver.eu/'
+    'https://excelextractor.randomwebserver.eu'
 ];
 
 module.exports = allowedOrigins;
