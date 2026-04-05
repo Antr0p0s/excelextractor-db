24918a09bc8edf1509aa7b775d24574e818530b1
Antr0p0s <jelmersijbers@gmail.com>
2026-04-05 16:43:09 +0200

    allowedorigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://excelextractor.randomwebserver.eu'
+    'https://excelextractor.randomwebserver.eu',
+    'https://pubquiz.randomwebserver.eu',
 ];
 
 module.exports = allowedOrigins; 
